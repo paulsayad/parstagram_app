094d92c4e72df8836842cf96797e79dd2188bacd
sayad <realPaulSayad@gmail.com>
2021-03-12 15:17:23 -0500

    Initial Commit

---
+++ parstagram_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  parstagram_app
+//
+//  Created by Paul Sayad on 3/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
