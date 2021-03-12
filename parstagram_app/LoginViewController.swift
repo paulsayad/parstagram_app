642bfb7a9441b5998e00eae1b58db571a71ab64d
sayad <realPaulSayad@gmail.com>
2021-03-12 17:28:25 -0500

    - Added login/create account - Added feed - Added option to upload images w/ captions

--- parstagram_app/LoginViewController.swift
+++ parstagram_app/LoginViewController.swift
@@ -20,6 +20,19 @@ class LoginViewController: UIViewController {
     }
     
     @IBAction func onSign(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func onSignup(_ sender: Any) {
         let user = PFUser()
         user.username = usernameField.text
         user.password = passwordField.text
@@ -27,11 +40,10 @@ class LoginViewController: UIViewController {
         user.signUpInBackground { (success, error) in
             if success {
                 self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: \(error?.localizedDescription)")
             }
         }
-    }
-    
-    @IBAction func onSignup(_ sender: Any) {
     }
     
     /*
